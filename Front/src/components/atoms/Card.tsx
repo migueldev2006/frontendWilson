b76a33a727090b4a3a763f0c966251ee8030d75b
migueldev2006 <gironorozcoluismiguel3@gmail.com>
2025-04-21 16:58:51 -0500

    Cargo modificaciones nuevas

--- Front/src/components/atoms/Card.tsx
+++ Front/src/components/atoms/Card.tsx
@@ -5,7 +5,7 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn("rounded-2xl border bg-white shadow-sm", className)}
+      className={cn("dark:bg-zinc-900 text-black dark:text-white bg-white shadow-sm", className)}
       {...props}
     />
   )
